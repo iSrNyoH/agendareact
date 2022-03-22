e0d15af79c86b596cc7f8bb02abd1f60dea43458
iSrNyoH <michael.hernandez5@outlook.es>
2022-03-21 20:44:54 -0400

    Fetch vew

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import './App.css';
 import 'styled-components'
 import React, {useState,useEffect} from 'react';
-import datatable, {createTheme} from 'react-data-table-component';
+import DataTable, {createTheme} from 'react-data-table-component';
 
 const App = () => {
+
+    const [users, setUsers] = useState([])
+
+    const URL = 'http://www.raydelto.org/agenda.php'
+    const showData = async () => {
+      const response = await fetch(URL)
+      const data = await response.json()
+      console.log(data)
+      setUsers(data)
+    }
+
+    useEffect( ()=>{
+      showData()
+    }, [])
+
+    const columnas =[
+      {
+        name: 'nombre',
+        selector: row => row.nombre
+      },
+      {
+        name: 'apellido',
+        selector: row => row.apellido
+      },
+      {
+        name: 'telefono',
+        selector: row => row.telefono
+      }
+    ]
 
 
 
   return (
     <div className="App">
-
+      <h1>Agenda de Contactos</h1>
+        <DataTable
+        columns={columnas}
+        data={users}
+        pagination
+        />
     </div>
   );
 }
